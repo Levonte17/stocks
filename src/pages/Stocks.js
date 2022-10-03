ad03c42bfb6ead7deff10747cd82035f133def4f
Levonte Larry <levontelarry@Levontes-MacBook-Air.local>
2022-10-03 08:11:55 -0400

    done with homework

--- src/pages/Stocks.js
+++ src/pages/Stocks.js
@@ -1,34 +1,36 @@
-
-import {useParams} from 'react-router-dom';
-import { useState, useEffect} from 'react';
-
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Stocks = (props) => {
-    
-const {symbol} = useParams();
-const [coin, setCoin] = useState(null);
+    const apiKey = "04e778150973ea4a6af60a69242db95d";
+    const { symbol } = useParams();
+    const url = `https://financialmodelingprep.com/api/v3/profile/${symbol}?apikey=${apiKey}`;
+    const [stock, setStock] = useState(null);
 
-const getStock = async () => {
-    const data = await Response.json();
-        setCoin(data);
-};
+    const getStock = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setStock(data);
+    };
+
     useEffect(() => {
-        setCoin();
+        getStock();
     }, []);
-    
-    const loaded = () => {
-        return(
-    <div>
-            <h1>{coin.price}</h1>
-            <h2>{coin.name}</h2>
-    </div>
 
-);
-}
-const loading = () => {
-    return <h1>loading...</h1>
-};
-return coin ? loaded() : loading();
+    const loaded = () => {
+        return (
+            <div>
+                <h1>Name: {stock[0].companyName}</h1>
+                <h2>Price: {stock[0].price}</h2>
+            </div>
+        );
+    };
+
+    const loading = () => {
+        return <h1>Loading...</h1>
+    };
+
+    return stock ? loaded() : loading();
 
 };
 
